74341ddc36f92f70936662420159941e6d6f7fbf
bowon95 <100118960+bowon95@users.noreply.github.com>
2023-07-27 23:12:56 +0900

    0727_ModifyHostScene

---
+++ Assets/Scripts/HostScene/SelectCrew.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectCrew : MonoBehaviour
+{
+    public GameObject[] vecCrews = new GameObject[6];
+    //SpriteRenderer m_Sprite;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+}
