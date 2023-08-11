c2072cb1ceb77a633dab3042bb04862428579375
bowon95 <100118960+bowon95@users.noreply.github.com>
2023-08-11 16:02:00 +0900

    0811_RaycastAll_SetImpPosToVent

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -5,47 +5,64 @@ using UnityEngine.UIElements;
 
 public class Arrow : MonoBehaviour
 {
+    GameObject m_Impostor;
+    GameObject m_DirObj;
 
+    Vector2 m_Dir;
+
+    
     void Start()
     {
+        m_Impostor = GameObject.FindWithTag("Impostor");
+
+       // m_DirObj = GameObject.FindWithTag("DirObj"); 
+        m_Dir = transform.GetChild(0).position - transform.position;
     }
 
     void Update()
     {
-        Debug.DrawRay(transform.position, Vector2.one * 10, new Color(1, 0, 0));
+        Debug.DrawRay(transform.position, m_Dir * 10, new Color(1, 0, 0));
 
-        RaycastHit2D Hit = Physics2D.Raycast(transform.position, Vector3.down, LayerMask.GetMask("Vent"));
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D[] Hits;
+            Hits = Physics2D.RaycastAll(transform.position, m_Dir, 10.0f, LayerMask.GetMask("Vent"));
 
-        if (Hit.collider != null)
-            Debug.Log(Hit.collider.name);
+            if (Hits.Length != null)
+            {
+                m_Impostor.transform.position = Hits[0].collider.transform.position;
+            }
 
-
-        // 짧은 선
-        //Debug.DrawRay(transform.position, Vector2.down, new Color(1,0,0));
-
-        //RaycastHit2D Hit = Physics2D.Raycast(transform.position, Vector3.down, LayerMask.GetMask("Vent"));
-
-        //if (Hit.collider != null)
-        //    Debug.Log(Hit.collider.name);
-
-
+            for (int i = 0; i < Hits.Length; ++i)
+            {
+                RaycastHit2D Hit = Hits[i];
 
 
+                Debug.Log(Hit.collider.gameObject.name);
+            }
+        }
+
+        //Debug.DrawRay(transform.position, Vector2.up * 10, new Color(1, 0, 0));
+
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    RaycastHit2D[] Hits;
+        //    Hits = Physics2D.RaycastAll(transform.position, Vector2.up, 10.0f, LayerMask.GetMask("Vent"));
+
+        //    if (Hits.Length != null)
+        //    {
+        //        m_Impostor.transform.position = Hits[0].collider.transform.position;
+        //    }
+
+        //    for (int i = 0; i < Hits.Length; ++i)
+        //    {
+        //        RaycastHit2D Hit = Hits[i];
 
 
+        //        Debug.Log(Hit.collider.gameObject.name);
+        //    }
+        //}
 
-
-
-
-        //Ray2D ray = new Ray2D(transform.position, transform.down);
-        //int layerMask = 1 << LayerMask.NameToLayer("Vent");
-
-        //RaycastHit2D HitInfo = Physics2D.Raycast(this.transform.down, this.transform.up, 10.0f, 1);// 충돌한 오브젝트 정보
-
-        //Debug.DrawRay(transform.position, transform.forward, Color.red); // 디버깅용
-
-        //if (HitInfo.collider != null)
-        //    Debug.Log(HitInfo.transform.name);
 
     }
 }
