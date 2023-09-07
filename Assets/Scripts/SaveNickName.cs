005c2d4e6e9d12a0b747430b5f1da946f962cdb6
bowon95 <100118960+bowon95@users.noreply.github.com>
2023-09-07 14:56:13 +0900

    0907_Complete_NickNameUI

---
+++ Assets/Scripts/SaveNickName.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public InputField m_InputNickName;
+    // Start is called before the first frame update
+
+    private void Update()
+    {
+        if (Input.GetKey("return"))
+            Save();
+
+    }
+
+    public void Save()
+    {
+        PlayerPrefs.SetString("NickName", m_InputNickName.text);
+    }
+
+    public void Load()
+    {
+        if (PlayerPrefs.HasKey("NickName"))
+        {
+            m_InputNickName.text = PlayerPrefs.GetString("NickName");
+        }
+    }
+}
+
