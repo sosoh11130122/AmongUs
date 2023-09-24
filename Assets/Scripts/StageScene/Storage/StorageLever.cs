3ec6e7c36bf675cee52dd56d83785f29ff119193
bowon95 <100118960+bowon95@users.noreply.github.com>
2023-09-24 17:32:15 +0900

    0924_VoteSystem_ing

--- Assets/Scripts/StageScene/Storage/StorageLever.cs
+++ Assets/Scripts/StageScene/Storage/StorageLever.cs
@@ -1,13 +1,16 @@
+using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class StorageLever : MonoBehaviour
+public class StorageLever : MonoBehaviourPunCallbacks
 {
     public Image m_LeverUp;
+    public GameObject m_Lever;
     Collider2D m_Box2D;
+    public PhotonView m_View;
 
 
     void Start()
@@ -17,8 +20,13 @@ public class StorageLever : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == "Player")
+        if (collision.gameObject.tag == "Player" && m_View.IsMine == true)
+        {
             m_LeverUp.gameObject.SetActive(true);
+            m_Lever.GetComponent<Collider2D>().enabled = false;
+        }
+        
+
     }
 
 }
